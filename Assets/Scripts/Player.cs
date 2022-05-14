677ebf0d85f63a8db26cd889583307ec3806f4a8
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-05-14 21:28:31 +0530

    Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    #region PUBLIC VARIABLES
+
+    #endregion
+    #region PRIVATE VARIABLES
+    private Vector3 playerTouchPosition;
+    private const string PLAYER_MOVEMENT_COROUTINE = "Player_Move_UpAndDown";
+    Rigidbody2D rigidbody2D;
+    #endregion
+    #region MONOBEHAVIOUR METHODS
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    private void OnEnable()
+    {
+        UserInput.UserInputHandler.OnTouchAction += ChangeCoordinates;
+    }
+    private void OnDisable()
+    {
+
+        UserInput.UserInputHandler.OnTouchAction -= ChangeCoordinates;
+    }
+    #endregion
+    #region PUBLIC METHODS
+    public void ChangeCoordinates(Touch touch)
+    {
+        playerTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        StopCoroutine(PLAYER_MOVEMENT_COROUTINE);
+        StartCoroutine(PLAYER_MOVEMENT_COROUTINE);
+
+    }
+    #endregion
+    #region PRIVATE METHODS
+    private IEnumerator Player_Move_UpAndDown()
     {
         
+        playerTouchPosition.z = this.transform.position.z; //Assigning z value of current position to touch position
+        playerTouchPosition.x=this.transform.position.x;//Assigning x value of current position to touch position
+
+        float distance = Vector3.Distance(transform.position, playerTouchPosition);
+        for (float i = 0; i < distance; i+=Time.deltaTime)
+        {
+            transform.position = Vector3.Lerp(transform.position, playerTouchPosition, i); // Transforming the current position slowly to the touch position 
+            yield return null;
+        }
+        transform.position = playerTouchPosition;
+        
+        
     }
+    #endregion
 }
---
+++ Assets/Scripts/UserInputHandler.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UserInput
+{
+    public class UserInputHandler : MonoBehaviour
+    {
+
+        #region Events
+        public delegate void TapAction(Touch touch);
+        public static event TapAction OnTouchAction;
+        #endregion
+        #region PUBLIC VARIABLES
+
+        #endregion
+        #region PRIVATE VARIABLES
+        private Vector2 movement;
+        float tapMaxMovement = 50f;
+        private bool tapGestureFailed = false;
+        #endregion
+        #region MONOBEHAVIOUR METHODS
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.touches[0];  // Need to find out no. of touches on the screen. If there are more no. of touches, need to call array
+                if (touch.phase == TouchPhase.Began) // We have several touch phases, began enters the first frame of the touch
+                {
+                    movement = Vector2.zero; //We made our movement to zero
+                }
+                else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                {
+                    movement += touch.deltaPosition; // The position delta since last change in pixel coordinates.
+                    if (movement.magnitude > tapMaxMovement)
+                    {
+                        tapGestureFailed = true;
+                        Debug.Log("Touch Failed");
+                    }
+                }
+                else
+                {
+                    if (!tapGestureFailed) // If finger is removed, then we are calling tap 
+                    {
+                        if (OnTouchAction != null)
+                        {
+                            OnTouchAction(touch);
+                        }
+
+                    }
+                    tapGestureFailed = false; // ready for the next tap
+
+                }
+
+            }
+        }
+        #endregion
+        #region PUBLIC METHODS
+
+        #endregion
+        #region PRIVATE METHODS
+
+        #endregion
+
+    }
+}
